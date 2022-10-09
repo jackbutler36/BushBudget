a11c30ed46f7afffa658735118c5192ec754e75a
jds7232 <78603667+jds7232@users.noreply.github.com>
2022-10-08 19:37:20 -0500

    fixing bug in resetting user password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :rememberable, :validatable
   def validate_phone_number # custom validation method for phone number
     if phone_number == nil
       errors.add(:phone_number, 'phone number cannot be empty')
