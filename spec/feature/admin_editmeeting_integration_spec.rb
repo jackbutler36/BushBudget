3bf76020a36a1342778944dcd391d54e92de4477
jds7232 <78603667+jds7232@users.noreply.github.com>
2022-11-03 16:43:50 -0500

    added integrations testing and removed scaffolding that was artificially lowering code coverage

--- spec/feature/admin_editmeeting_integration_spec.rb
+++ spec/feature/admin_editmeeting_integration_spec.rb
@@ -1,46 +1,39 @@
 require 'rails_helper'
 
+# Example
 RSpec.describe 'Running admin_meeting/edit integration tests', type: :feature do
-    def set_defaults # setting parameters without validation for integration testing
-        fill_in 'First name', with: 'Jonas'
-        fill_in 'Last name', with: 'Stites'
-        fill_in 'Street address', with: '711 University Dr'
-        fill_in 'Street address line two', with: '1122'
-        fill_in 'City', with: 'College Station'
-        select 'TX', from: 'State'
-        fill_in 'Uin', with: '123456789'
-        select 'member', from: 'Position'
-        fill_in 'Committee', with: 'R&D'
-        fill_in 'Email', with: 'bushtest@gmail.com'
-        fill_in 'Password', with: 'pass1234'
-        fill_in 'Password confirmation', with: 'pass1234'
-        fill_in 'Excusal date', with: Date.new(Time.now.year, Time.now.month, Time.now.day)
-      end
-    
-      def set_defaults_meeting
-        fill_in 'Description', with: 'Meeting about R&D'
-        fill_in 'Date', with: Date.new(2022,12,1)
-        fill_in 'Password', with: 'goodpassword'
-      end
-    
-      def new_meeting(date, password)
-        meeting = Meeting.new
-        meeting.date = Date.new(2022,12,1)
-        meeting.description = 'testing meeting functionality'
-        meeting.password = password
-        meeting.save!
-      end
-    
-      def new_admin(email, password) # creates new admin account for testing
-        admin = Admin.new
-        admin.email = email
-        admin.password = password
-        admin.password_confirmation = password
-      end
-    
       def login(email, password)
         fill_in 'Email', with: email
         fill_in 'Password', with: password
         click_on 'Log in'
+      end
+
+      scenario 'go to meetings, users, dashboard main page' do
+        admin = Admin.new
+        admin.email = 'tamubushtest@gmail.com'
+        admin.password = 'bushboys512'
+        admin.password_confirmation = 'bushboys512'
+        admin.save!
+        visit new_admin_session_path
+        login('tamubushtest@gmail.com', 'bushboys512')
+        meeting = Meeting.new
+        meeting.date = Date.new(2022,12,1)
+        meeting.description = 'testing meeting functionality'
+        meeting.password = 'pass123'
+        meeting.save!
+        visit edit_meeting_path(meeting)
+        click_on 'Meetings'
+        expect(page).to have_content('Meetings')
+        visit edit_meeting_path(meeting)
+        click_on 'Users'
+        expect(page).to have_content('Users')
+        visit edit_meeting_path(meeting)
+        click_on 'Dashboard'
+        expect(page).to have_content('Dashboard')
+        visit edit_meeting_path(meeting)
+        fill_in 'Password', with: 'pass1234'
+        click_on 'Update Meeting'
+        visit meetings_path
+        expect(page).to have_content('pass1234')
       end
 end
