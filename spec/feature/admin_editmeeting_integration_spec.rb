43d13d0dd9b58a05e551d15e32aa2ccf0d1fcb33
Himel Mondal <you@example.com>
2022-11-03 10:08:51 -0500

    created navbar

--- spec/feature/admin_editmeeting_integration_spec.rb
+++ spec/feature/admin_editmeeting_integration_spec.rb
@@ -36,55 +36,11 @@ RSpec.describe 'Running admin_meeting/edit integration tests', type: :feature do
         admin.email = email
         admin.password = password
         admin.password_confirmation = password
-        admin.save!
-      end
-    
-      def new_attendance(uin, password)
-        attendance = Attendance.new
-        attendance.userNum = uin
-        attendance.password = password
-        attendance.save!
-      end
-    
-      def new_user(email, password) # creates new admin account for testing
-        user = User.new
-        user.first_name =  'Test'
-        user.last_name =  'User'
-        user.street_address = '711 uni dr'
-        user.street_address_line_two = '1320'
-        user.city = 'College Station'
-        user.state = 'TX'
-        user.zip_code = '77840'
-        user.phone_number = '(222)333-4444'
-        user.uin = '111222333'
-        user.position = 'member'
-        user.committee = 'R&D'
-        user.email =  email
-        user.password = password
-        user.password_confirmation = password
-        user.excusal_date = Date.new(Time.now.year, Time.now.month, Time.now.day)
-        user.save!
       end
     
       def login(email, password)
         fill_in 'Email', with: email
         fill_in 'Password', with: password
         click_on 'Log in'
-      end
-
-      scenario 'go from edit back to dashboard' do # navigating from edit to dashboard admin
-        new_admin('tamubushtest@gmail.com', 'bushboys512')
-        new_meeting(Date.new(2022,12,1), 'pass456')
-        new_meeting(Date.new(2022,12,4), 'pass789')
-        new_meeting(Date.new(2022,12,7), 'pass012')
-        visit new_admin_session_path
-        login('tamubushtest@gmail.com', 'bushboys512')
-        click_on 'Meetings'
-        click_on 'Edit'
-        set_defaults_meeting
-        click_on 'Dashboard'
-        
-        expect(page).to have_content('Admin Dashboard')
-       
       end
 end
