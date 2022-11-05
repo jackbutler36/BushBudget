7777feb504ec818bd8e00d85e611329fc87eb867
jds7232 <78603667+jds7232@users.noreply.github.com>
2022-11-05 18:59:29 -0500

    adding failings tests for users stories as part of TDD

---
+++ spec/feature/new_user_integration_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+# Example
+RSpec.describe 'Running admin_meeting/edit integration tests', type: :feature do
+      def login(email, password)
+        fill_in 'Email', with: email
+        fill_in 'Password', with: password
+        click_on 'Log in'
+      end
+
+      scenario 'go to meetings, users, dashboard main page' do
+        admin = Admin.new
+        admin.email = 'tamubushtest@gmail.com'
+        admin.password = 'bushboys512'
+        admin.password_confirmation = 'bushboys512'
+        admin.save!
+        visit new_admin_session_path
+        login('tamubushtest@gmail.com', 'bushboys512')
+        
+        visit new_user_path
+        click_on 'Meetings'
+        expect(page).to have_content('Meetings')
+        visit new_user_path
+        click_on 'Users'
+        expect(page).to have_content('Users')
+        visit new_user_path
+        click_on 'Dashboard'
+        expect(page).to have_content('Dashboard')
+      end
+end
